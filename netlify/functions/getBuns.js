e8aee5380879e000aa780015bc545fc6a43b881c
jenny keener <jenny@Jennys-MBP.localdomain>
2025-04-11 15:59:15 -0400

    Add getBuns function

---
+++ netlify/functions/getBuns.js
@@ -0,0 +1,32 @@
+const { Client } = require('@notionhq/client');
+
+exports.handler = async function(event, context) {
+  const notion = new Client({ auth: process.env.NOTION_TOKEN });
+
+  const databaseId = process.env.NOTION_DB_ID;
+
+  try {
+    const response = await notion.databases.query({
+      database_id: databaseId,
+    });
+
+    const results = response.results.map(page => {
+      const title = page.properties.Name.title[0]?.plain_text || "Untitled";
+      const location = page.properties.Location.rich_text[0]?.plain_text || "";
+      const note = page.properties.Review.rich_text[0]?.plain_text || "";
+
+      return { title, location, note };
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
